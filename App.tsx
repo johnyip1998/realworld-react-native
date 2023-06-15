2eb2323ae9a279ac02021f44a314c39335f93428
Chun On YIP <yip4work@gmail.com>
2023-06-15 12:02:42 +0800

    chore: switch using yarn

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>I am running this project with pnpm package manager.</Text>
+      <Text>I am running this project with yarn package manager.</Text>
       <StatusBar style="auto" />
     </View>
   );
